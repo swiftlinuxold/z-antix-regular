bb7012bd6d9ef9ceff5dbfb535d2b2b879c67f0e
Jason Hsu <github@jasonhsu.com>
2011-08-10 18:20:44 -0500

    First commit

---
+++ conky.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import os # allows interaction with the operating system
+import getpass # allows the username to be obtained
+import os.path # allows you to determine if a directory exists
+
+is_chroot = os.path.exists("/srv")
+username=''
+dir_develop=''
+
+if (is_chroot):
+	dir_develop='/usr/local/bin/develop'
+else:
+	username=os.environ['XAUTHORITY']
+	username=username[6:-12]
+	dir_develop='/home/'+username+'/develop'
+	
+if (~is_chroot):
+	# Replace "Diet" with "Regular"
+    text=open('/home/'+username+'/.conkyrc', 'r').read()
+    text = text.replace("Diet", "Regular") 
+    open('/home/'+username+'/.conkyrc', "w").write(text)
+    
+if (~is_chroot):
+	# Replace "Diet" with "Regular"
+    text=open('/etc/skel/.conkyrc', 'r').read()
+    text = text.replace("Diet", "Regular") 
+    open('/etc/skel/.conkyrc', "w").write(text)
+	
---
+++ main.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Proper header for a Bash script.
+
+# Check for root user login
+if [ ! $( id -u ) -eq 0 ]; then
+	echo "You must be root to run this script."
+	echo "Please enter su before running this script again."
+	exit 2
+fi
+
+IS_CHROOT=0 # changed to 1 if and only if in chroot mode
+USERNAME=""
+DIR_DEVELOP=""
+
+# The remastering process uses chroot mode.
+# Check to see if this script is operating in chroot mode.
+# /srv directory only exists in chroot mode
+if [ -d "/srv" ]; then
+	IS_CHROOT=1 # in chroot mode
+	DIR_DEVELOP=/usr/local/bin/develop 
+else
+	USERNAME=$(logname) # not in chroot mode
+	DIR_DEVELOP=/home/$USERNAME/develop 
+fi
+
+# From Diet Swift Linux to Regular Swift Linux
+
+# Change Conky
+su -c "python $DIR_DEVELOP/regular/conky.py" $USERNAME
+
+# Change MIME-types
+
+# Change ROX
+
+# Add OpenOffice
+
+# Add forensic packages
+
+
+
+
+
+
+
+
+
+# Replacing files in /usr/local/bin and /usr/share/antiX/localisation/en/local-bin
+# Remove the idesk, Fluxbox, and Nitrogen configuration options 
+# (due to the removal of these packages)
+# antiX Control Center -> Swift Linux Control Center
+
+echo "REPLACING THE CONTROL CENTER"
+rm /usr/local/bin/antixcc.sh
+cp $DIR_DEVELOP/control_center/usr_local_bin/antixcc.sh /usr/local/bin
+chown root:root /usr/local/bin/antixcc.sh
+chmod a+x /usr/local/bin/antixcc.sh
+
+rm /usr/share/antiX/localisation/en/local-bin/antixcc.sh
+cp $DIR_DEVELOP/control_center/usr_local_bin/antixcc.sh /usr/share/antiX/localisation/en/local-bin
+chown root:root /usr/share/antiX/localisation/en/local-bin/antixcc.sh
+chmod a+x /usr/share/antiX/localisation/en/local-bin/antixcc.sh
+
+if [ $IS_CHROOT -eq 0 ]; then
+	chown $USERNAME:users /usr/share/antiX/localisation/en/local-bin/antixcc.sh
+else
+	chown demo:users /usr/share/antiX/localisation/en/local-bin/antixcc.sh
+fi
+
+exit 0
